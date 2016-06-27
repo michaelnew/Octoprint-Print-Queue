1ef449cfb3e90f5b9d7a4fb35eed49ff8e0b1c8d
Mike New <mikenew12@gmail.com>
2016-06-27 13:54:24 -0700

    fixed print finish handler

--- octoprint_material_settings/__init__.py
+++ octoprint_material_settings/__init__.py
@@ -34,13 +34,13 @@ class MaterialSettingsPlugin(octoprint.plugin.StartupPlugin,
 # SettingsPlugin
     def get_settings_defaults(self):
         return dict(bed_temp="50",
-        	print_temp="200")
+            print_temp="200")
 
 # TemplatePlugin
     def get_template_vars(self):
         return dict(
-        	bed_temp=self._settings.get(["bed_temp"]),
-        	print_temp=self._settings.get(["print_temp"]))
+            bed_temp=self._settings.get(["bed_temp"]),
+            print_temp=self._settings.get(["print_temp"]))
 
     def get_template_configs(self):
         return [
@@ -49,8 +49,8 @@ class MaterialSettingsPlugin(octoprint.plugin.StartupPlugin,
 
 # AssetPlugin
     def get_assets(self):
-     	return dict(
-         	js=["js/material_settings.js"]
+        return dict(
+            js=["js/material_settings.js"]
     )
 
 # Data Persistence
@@ -82,13 +82,16 @@ class MaterialSettingsPlugin(octoprint.plugin.StartupPlugin,
         self._materials_dict = result_dict
         return result_dict
 
-    def print_completion_script(comm, script_type, script_name, *args, **kwargs):
-	    if script_type == "gcode" and script_name == "afterPrintDone":
-		    prefix = "M104 S0\nM140 S0\nG1 Y215 F3000\nG1 X107 F3000\nM106 S255\nM190 S28\nM140 S0\nG1 Z12\nG1 Y0 F3000\nG1 Y215 F8000"
-		    postfix = None
-		    return prefix, postfix
-		else:
-			return None
+    def print_completion_script(self, comm, script_type, script_name, *args, **kwargs):
+        self._logger.info("MSL: print completion handler called")
+        if script_type == "gcode" and script_name == "afterPrintDone":
+            self._logger.info("MSL: script type matched. prepending gcode")
+            prefix = "M104 S0\nM140 S0\nG1 Y215 F3000\nG1 X107 F3000\nM106 S255\nM190 S28\nG1 Z12\nG1 Y0 F3000\nG1 Y215 F8000"
+            postfix = None
+            return prefix, postfix
+        else:
+            self._logger.info("MSL: script type did not match")
+            return None
 
 # Gcode replacement
     def set_bed_temp(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
@@ -96,26 +99,26 @@ class MaterialSettingsPlugin(octoprint.plugin.StartupPlugin,
         pTempKey = self._settings.get(["print_temp"])
 
         if cmd:
-	    	if cmd[:8] == ("M190 S" + bTempKey):
-	            t = self._materials_dict["bed_temp"]
-	            if t and t != "":
-	                cmd = "M190 S" + t
-	                return cmd
-	    	if cmd[:8] == ("M140 S" + bTempKey):
-	            t = self._materials_dict["bed_temp"]
-	            if t and t != "":
-	                cmd = "M140 S" + t
-	                return cmd
-	    	if cmd[:9] == ("M104 S" + pTempKey):
-	            t = self._materials_dict["print_temp"]
-	            if t and t != "":
-	                cmd = "M104 S" + t
-	                return cmd
-	    	if cmd[:9] == ("M109 S" + pTempKey):
-	            t = self._materials_dict["print_temp"]
-	            if t and t != "":
-	                cmd = "M109 S" + t
-	                return cmd
+            if cmd[:8] == ("M190 S" + bTempKey):
+                t = self._materials_dict["bed_temp"]
+                if t and t != "":
+                    cmd = "M190 S" + t
+                    return cmd
+            if cmd[:8] == ("M140 S" + bTempKey):
+                t = self._materials_dict["bed_temp"]
+                if t and t != "":
+                    cmd = "M140 S" + t
+                    return cmd
+            if cmd[:9] == ("M104 S" + pTempKey):
+                t = self._materials_dict["print_temp"]
+                if t and t != "":
+                    cmd = "M104 S" + t
+                    return cmd
+            if cmd[:9] == ("M109 S" + pTempKey):
+                t = self._materials_dict["print_temp"]
+                if t and t != "":
+                    cmd = "M109 S" + t
+                    return cmd
 
 __plugin_name__ = "Material Settings"
 def __plugin_load__():
