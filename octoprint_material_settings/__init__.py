0fd2797a36521722a228c560f79d4ae7d3768845
Mike New <mikenew12@gmail.com>
2016-06-27 11:36:39 -0700

    added test script

--- octoprint_material_settings/__init__.py
+++ octoprint_material_settings/__init__.py
@@ -82,31 +82,40 @@ class MaterialSettingsPlugin(octoprint.plugin.StartupPlugin,
         self._materials_dict = result_dict
         return result_dict
 
+    def print_completion_script(comm, script_type, script_name, *args, **kwargs):
+	    if script_type == "gcode" and script_name == "afterPrintDone":
+		    prefix = "M104 S0\nM140 S0\nG1 Y215 F3000\nG1 X107 F3000\nM106 S255\nM190 S28\nM140 S0\nG1 Z12\nG1 Y0 F3000\nG1 Y215 F8000"
+		    postfix = None
+		    return prefix, postfix
+		else:
+			return None
+
 # Gcode replacement
     def set_bed_temp(self, comm_instance, phase, cmd, cmd_type, gcode, *args, **kwargs):
         bTempKey = self._settings.get(["bed_temp"])
         pTempKey = self._settings.get(["print_temp"])
 
-    	if cmd and cmd[:8] == ("M190 S" + bTempKey):
-            t = self._materials_dict["bed_temp"]
-            if t and t != "":
-                cmd = "M190 S" + t
-                return cmd
-    	if cmd and cmd[:8] == ("M140 S" + bTempKey):
-            t = self._materials_dict["bed_temp"]
-            if t and t != "":
-                cmd = "M140 S" + t
-                return cmd
-    	if cmd and cmd[:9] == ("M104 S" + pTempKey):
-            t = self._materials_dict["print_temp"]
-            if t and t != "":
-                cmd = "M104 S" + t
-                return cmd
-    	if cmd and cmd[:9] == ("M109 S" + pTempKey):
-            t = self._materials_dict["print_temp"]
-            if t and t != "":
-                cmd = "M109 S" + t
-                return cmd
+        if cmd:
+	    	if cmd[:8] == ("M190 S" + bTempKey):
+	            t = self._materials_dict["bed_temp"]
+	            if t and t != "":
+	                cmd = "M190 S" + t
+	                return cmd
+	    	if cmd[:8] == ("M140 S" + bTempKey):
+	            t = self._materials_dict["bed_temp"]
+	            if t and t != "":
+	                cmd = "M140 S" + t
+	                return cmd
+	    	if cmd[:9] == ("M104 S" + pTempKey):
+	            t = self._materials_dict["print_temp"]
+	            if t and t != "":
+	                cmd = "M104 S" + t
+	                return cmd
+	    	if cmd[:9] == ("M109 S" + pTempKey):
+	            t = self._materials_dict["print_temp"]
+	            if t and t != "":
+	                cmd = "M109 S" + t
+	                return cmd
 
 __plugin_name__ = "Material Settings"
 def __plugin_load__():
@@ -116,4 +125,5 @@ def __plugin_load__():
     global __plugin_hooks__
     __plugin_hooks__ = {
         "octoprint.comm.protocol.gcode.queuing": __plugin_implementation__.set_bed_temp,
+        "octoprint.comm.protocol.scripts": __plugin_implementation__.print_completion_script,
     }
